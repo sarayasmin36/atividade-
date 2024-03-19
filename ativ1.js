8e0ef42391ba448fcd1959d1561c07c0c4473b7d
Sara <sara.y@escolar.ifrn.edu.br>
2024-03-19 16:33:32 -0300

    ytcvitykh

--- ativ1.js
+++ ativ1.js
@@ -1,5 +1,20 @@
-// s1. Crie uma função que calcule a idade de uma pessoa com base na data de nascimento fornecida em um campo de formulário.
-let ano = currentDate.getFullYear();
-let month = currentDate.getMonth();
-let day = currentDate.getDay();
 
+function calcularIdade2() {
+    let dataNascimento = new Date(document.querySelector("#data2").value);
+    let diaNascimento = dataNascimento.getDate();
+    let mesNascimento = dataNascimento.getMonth() + 1;
+    let anoNascimento = dataNascimento.getFullYear();
+
+    let hoje = new Date();
+    let diaAtual = hoje.getDate();
+    let mesAtual = hoje.getMonth() + 1;
+    let anoAtual = hoje.getFullYear();
+
+    let idade = anoAtual - anoNascimento;
+
+    if (mesAtual < mesNascimento || 
+       (mesAtual === mesNascimento && diaAtual < diaNascimento)) {
+        idade--;
+    }
+    document.querySelector("#idade").innerHTML = `Idade: ${idade}`;
+}
