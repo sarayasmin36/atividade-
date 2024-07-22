2d8c4f17fd79f77165e7984102251d6bebb9ee64
Sara <sara.y@escolar.ifrn.edu.br>
2024-07-22 07:21:32 -0300

    aaaa

---
+++ revisao/q20.js
@@ -0,0 +1,33 @@
+function cadastrar() {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha1 = document.getElementById("senha1").value;    
+    const senha2 = document.getElementById("senha2").value;
+    
+    //const regex = /^(?=.*[a-zA-Z\d]).{8,}$/;
+    let resultado = document.getElementById("resultado");
+
+    if (!nome)
+        resultado.innerHTML = '<b>Preencha o nome!</b>';
+    else if (!email)
+        resultado.innerHTML = '<b>Preencha o e-mail!</b>';
+    else if (!senha1 || !senha2)
+        resultado.innerHTML = '<b>Preencha a senha!</b>';
+    else if (senha1 != senha2)
+        resultado.innerHTML = '<b>Senhas divergentes!</b>';
+    else if (!email.includes('@') || !email.includes('.'))
+        resultado.innerHTML = '<b>E-mail inválido!</b>';
+    //else if (!regex.test(senha1))
+    else if (!validarSenha(senha1))
+        resultado.innerHTML = '<b>Senha não segue as regras!</b>';
+    else {
+        resultado.innerHTML = '<b>Cadastro realizado com sucesso!</b>';
+    }
+}
+
+function validarSenha(senha) {
+    var temLetras = /[a-zA-Z]/.test(senha); // uso de expressão regular
+    var temNumeros = /\d/.test(senha);
+    var tamanhoValido = senha.length >= 8;
+    return temLetras && temNumeros && tamanhoValido;
+}
