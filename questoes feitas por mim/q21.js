8b5cc55da1e7ceaf472746ab5762d7adedc53dc9
Sara <sara.y@escolar.ifrn.edu.br>
2024-07-28 20:24:34 -0300

    ccccccccc

--- questoes feitas por mim/q21.js
+++ questoes feitas por mim/q21.js
@@ -1,9 +1,9 @@
-function descricao() {
+function adicionar() {
 
     const nome = document.getElementById('nome').value;
-    const preço = document.getElementById('preço').value;
-    const  categoria = document.getElementById('categoria').value;
-    const  descrição = document.getElementById('descrição').value;
+    const preço = document.getElementById('preco').value;
+    const categoria = document.getElementById('categoria').value;
+    const descrição = document.getElementById('descricao').value;
 
     if ( !nome || !preço || !categoria || !descrição ) {
         alert('Por favor, preencha todos os campos.');
@@ -11,10 +11,10 @@ function descricao() {
     }
 
     let linha = document.getElementById('tabelaProdutos').insertRow();
-    linha.insertcell(0).textContent = nome;
-    linha.insertcell(1).textContent = preço;
-    linha.insertcell(2).textContent = categoria;
-    linha.insertcell(3).textContent = descrição;
+    linha.insertCell(0).textContent = nome;
+    linha.insertCell(1).textContent = preço;
+    linha.insertCell(2).textContent = categoria;
+    linha.insertCell(3).textContent = descrição;
 
     document.getElementById('formProduto').reset();
 }
