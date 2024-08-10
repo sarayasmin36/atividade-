5b5036f077d47e20c99e6167feda61faf0c0947c
SEU_NOME_DE_USUARIO <sara.y@escolar.ifrn.edu.br>
2024-08-10 15:29:50 -0300

    dfftgg

---
+++ codiho.js
@@ -0,0 +1,20 @@
+const numeros = [2, 3, 7, 8, 10]
+console.log(numeros.toString());
+
+const qnumero = numeros.map(function(numero){
+    return numero ** 2;
+});
+console.log(qnumero.toString());
+
+let resultado = numeros.filter(function(numeros){
+    return numeros > 5;
+})
+console.log(resultado.toString());
+
+resultado = numeros.reduce(function(total, numeros){
+    return total + numeros;
+}, 0);
+console.log(resultado.toString());
+
+
+
